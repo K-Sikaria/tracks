7c1f0ef457da5dc417c40bf2d0b9e36898f23e7a
K-Sikaria <khushisikaria1@gmail.com>
2023-08-14 18:07:04 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -2,36 +2,42 @@ import React, { useState, useEffect } from 'react';
 import TrackCard from './Trackcards';
 
 const App = () => {
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
 
   useEffect(() => {
-    const handleResize = () => {
-      const screenWidth = window.innerWidth;
-      setIsDesktop(screenWidth >= 768);
-    };
-
     window.addEventListener('resize', handleResize);
+
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <div className="relative min-h-screen flex justify-end items-end">
-      {/* Background */}
-      <div className="absolute inset-0 z-[-1]">
-        {isDesktop ? (
-          <img src="/images/desktop_bg.png" alt="Desktop Background" className="object-cover w-full h-full" />
-        ) : (
-          <img src="/images/mobile_bg.png" alt="Mobile Background" className="object-cover w-full h-full" />
-        )}
-      </div>
-
-      <div className="absolute inset-0 flex flex-col items-end justify-center pb-8 pr-4 md:pb-12 md:pr-8 lg:pb-16 lg:pr-12 xl:pb-20 xl:pr-16">
-        <TrackCard title="TRACK 01" icon="/images/scholar.png" positionRight={true} isDesktop={isDesktop} />
-        <TrackCard title="TRACK 02" icon="/images/medical.png" positionRight={false} isDesktop={isDesktop} />
-        <TrackCard title="TRACK 03" icon="/images/education.png" positionRight={true} isDesktop={isDesktop} />
-        <TrackCard title="TRACK 04" icon="/images/currency.png" positionRight={false} isDesktop={isDesktop} />
+    <div
+      className="relative h-screen"
+      style={{
+        backgroundImage: isMobile
+          ? "url('/images/mobile_bg.png')"
+          : "url('/images/desktop_bg.png')",
+        backgroundSize: '100% 100%',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div
+        className={`absolute ${
+          isMobile
+            ? 'top-[22%] right-[40%] w-1 h-1/4'
+            : 'top-[15%] left-2/3 h-1/2 md:top-[13%] md:left-3/4 md:transform md:-translate-x-1/2 md:h-1/4 lg:left-[80%] lg:h-1/8 lg:top:[10%'
+        } flex flex-col gap-0 md:gap-4 lg:gap-0`}
+      >
+        <TrackCard title="TRACK 01" icon="/images/scholar.png" positionRight={true}  />
+        <TrackCard title="TRACK 02" icon="/images/medical.png" positionRight={false}  />
+        <TrackCard title="TRACK 03" icon="/images/education.png" positionRight={true}  />
+        <TrackCard title="TRACK 04" icon="/images/currency.png" positionRight={false}  />
       </div>
     </div>
   );
