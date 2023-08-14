027be29e7e4d03e561e5bab937cf8da4b5424641
K-Sikaria <khushisikaria1@gmail.com>
2023-08-14 13:53:54 +0530

    tracks commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import TrackCard from './Trackcards';
 
-function App() {
+const App = () => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      setIsDesktop(screenWidth >= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative min-h-screen flex justify-end items-end">
+      {/* Background */}
+      <div className="absolute inset-0 z-[-1]">
+        {isDesktop ? (
+          <img src="/images/desktop_bg.png" alt="Desktop Background" className="object-cover w-full h-full" />
+        ) : (
+          <img src="/images/mobile_bg.png" alt="Mobile Background" className="object-cover w-full h-full" />
+        )}
+      </div>
+
+      <div className="absolute inset-0 flex flex-col items-end justify-center pb-8 pr-4 md:pb-12 md:pr-8 lg:pb-16 lg:pr-12 xl:pb-20 xl:pr-16">
+        <TrackCard title="TRACK 01" icon="/images/scholar.png" positionRight={true} isDesktop={isDesktop} />
+        <TrackCard title="TRACK 02" icon="/images/medical.png" positionRight={false} isDesktop={isDesktop} />
+        <TrackCard title="TRACK 03" icon="/images/education.png" positionRight={true} isDesktop={isDesktop} />
+        <TrackCard title="TRACK 04" icon="/images/currency.png" positionRight={false} isDesktop={isDesktop} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
